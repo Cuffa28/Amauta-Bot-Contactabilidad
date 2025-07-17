c81e6a880f1f62e4f457c7874190721dd4efee98
Cuffa28 <facundo@amautainversiones.com>
2025-07-17 16:47:55 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import datetime
 import re
-from drive_utils import obtener_hoja_clientes, normalizar
+from drive_utils import normalizar
 
 def detectar_tipo(frase):
     frase = frase.lower()
@@ -38,27 +38,3 @@ def extraer_datos(frase):
 
     raise ValueError("No se pudo interpretar la frase. Usá el formato sugerido.")
 
-def buscar_clientes_similares_por_asesor(cliente_input, asesor_input):
-    df_clientes = obtener_hoja_clientes()
-    nombres = df_clientes["CLIENTE"].tolist()
-    asesores = df_clientes["ASESOR/A"].tolist()
-
-    cliente_input_normalizado = normalizar(cliente_input)
-    coincidencias = []
-    coincidencia_exacta = None
-
-    for i, (nombre, asesor) in enumerate(zip(nombres, asesores), start=2):
-        if asesor != asesor_input:
-            continue
-
-        nombre_normalizado = normalizar(nombre)
-
-        if cliente_input_normalizado == nombre_normalizado:
-            coincidencia_exacta = (i, nombre)
-            break
-        elif cliente_input_normalizado in nombre_normalizado or nombre_normalizado in cliente_input_normalizado:
-            coincidencias.append((i, nombre))
-
-    if coincidencia_exacta:
-        return [coincidencia_exacta]
-    return coincidencias
