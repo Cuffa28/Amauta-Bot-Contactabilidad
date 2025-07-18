46788819bf2d4d9a1df0870b3c938f67a2fe6196
Cuffa28 <facundo@amautainversiones.com>
2025-07-18 16:21:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ with tabs[1]:
             if cols[1].button("✔️ Hecho", key=f"recordatorio_hecho_{i}"):
                 try:
                     marcar_contacto_como_hecho(cliente, asesor)
-                    st.experimental_rerun()
+                    st.rerun()
                 except Exception as e:
                     st.error(f"⚠️ {e}")
     else:
