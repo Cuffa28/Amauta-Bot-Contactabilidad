11f6589bdb73e613f5ef0ad1d716e6150f618136
Cuffa28 <facundo@amautainversiones.com>
2025-07-18 16:54:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ if vencen_hoy and not st.session_state.popup_oculto:
         </button>
     </div>
     """
-    html(contenido_popup, height=300)
+    html(contenido_popup, height=10)
 
 tabs = st.tabs(["📞 Cargar Contactos", "📅 Recordatorios Pendientes"])
 
